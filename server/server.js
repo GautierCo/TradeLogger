cd8cbe2117b0505599fd7786019bc025711fab24
GautierCo <gautier.colasse@gmail.com>
2021-01-21 11:48:11 +0100

    Add front verifications on forms (trade)

--- server/server.js
+++ server/server.js
@@ -1,9 +1,6 @@
 require("dotenv").config();
 require("./config/db");
-
-const moment = require("moment");
-const duration = require("moment-duration-format");
-
+const helmet = require("helmet");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -14,8 +11,6 @@ const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user.routes");
 const authRoutes = require("./routes/auth.routes");
 const tradeRoutes = require("./routes/trade.routes");
-
-const { calculProfit, calculProfitPer, knowStatus, calculSessionDuration } = require("./utils/utils");
 
 // Middlewares
 app.use(
@@ -29,31 +24,12 @@ app.use(
     })
 );
 
+app.use(helmet());
+app.disable("x-powered-by");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-/* TEST Calculs */
-const profit = calculProfit({ type: "Long", entryPrice: 1.5, exitPrice: 2.0, leverage: 1, capital: 100, fees: 10 });
-const profitPer = calculProfitPer(profit, 100);
-const status = knowStatus(profit);
-
-//console.log(profit, profitPer, status);
-
-/* TEST Dates 
-const sessionDuration = calculSessionDuration(
-    "Fri Jan 15 2021 22:00:00 GMT+0100 (heure normale d’Europe centrale)",
-    "Fri Jan 22 2021 22:00:00 GMT+0100 (heure normale d’Europe centrale)"
-);
-*/
-
-const seconds = "18260";
-const durationInSeconds = moment.duration(seconds, "seconds");
-const sessionDuration = durationInSeconds.format(duration, "dd:hh:mm:ss");
-
-console.log(sessionDuration);
-//console.log(sessionDuration);
-/* */
 
 // Routes
 
